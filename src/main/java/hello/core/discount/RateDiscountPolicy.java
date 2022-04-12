b90304a0bc03a1bb92c21ca6b8687de42cfc5f6a
YangByoungSeok <ybs991@naver.com>
2022-04-12 16:05:29 +0900

    Chapter 2. 관심사의 분리

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPersent = 10;
+
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPersent/ 100;
+        }
+        return 0;
+    }
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,23 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy =  new FixDiscountPolicy();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy =  new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy =  new RateDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
 
+    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
