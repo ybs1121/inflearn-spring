68be8c2d57f616456e3bf3c3b9951f6563ed2013
ybs <ybs991@naver.co>
2022-04-27 15:43:48 +0900

    Chapter 9

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long,Member> store = new HashMap<>();
     @Override
